8a0fe8f3b68f1a843e546ce43e7df42392952318
Harsha Kotta <hk3367@columbia.edu>
2025-07-13 18:08:44 -0400

    kotta_min_sub: added a subscriber that subscribes to the /scan topic

--- kotta_min_sub/src/minimal_subscriber.rs
+++ kotta_min_sub/src/minimal_subscriber.rs
@@ -6,10 +6,16 @@ fn main() -> Result<(), Error> {
     let mut executor = context.create_basic_executor();
     let node = executor.create_node("minimal_subscriber")?;
     let worker = node.create_worker::<usize>(0);
-    let _subscription = worker.create_subscription::<nav_msgs::msg::Odometry, _>(
+    let node_clone_odom = node.clone();
+    let _odom_subscription = worker.create_subscription::<nav_msgs::msg::Odometry, _>(
         "/odom",
-        move |msg: nav_msgs::msg::Odometry| {
-            log!(node.logger(), "Odom Position:{:?}", msg)
+        move |_odom: nav_msgs::msg::Odometry| log!(node_clone_odom.logger(), "got odom"),
+    )?;
+    let node_clone_scan = node.clone();
+    let _lidar_subscription = worker.create_subscription::<sensor_msgs::msg::LaserScan, _>(
+        "/scan",
+        move |_lidar_scan: sensor_msgs::msg::LaserScan| {
+            log!(node_clone_scan.logger(), "got lidar scan")
         },
     )?;
 
